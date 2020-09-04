997ea18f670c06e11a8280d29c068ce82772535e
ashokide <kbnashok@gmail.com>
2020-09-05 04:49:53 +0530

    Updates

---
+++ precache-manifest.2d0ef902a97d394fb3af4b5e1f3a479d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1cd111dee06ff9e296b28bd4d41627",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "33ecc7709c525b4d634a",
+    "url": "/react-todolist/static/css/main.30d84ccb.chunk.css"
+  },
+  {
+    "revision": "7ac22cef7cfb93ba874c",
+    "url": "/react-todolist/static/js/2.5e15d667.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.5e15d667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ecc7709c525b4d634a",
+    "url": "/react-todolist/static/js/main.979f3fa8.chunk.js"
+  },
+  {
+    "revision": "dea162c310a378ff9d0d",
+    "url": "/react-todolist/static/js/runtime-main.82f4965a.js"
+  }
+]);
